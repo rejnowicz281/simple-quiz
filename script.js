ebf99941a58a56cf5ead6e7e4c18fddd8039968d
Alan <alanrejnowicz281@gmail.com>
2020-03-30 21:35:01 +0200

    Added a script that makes it so that when you click on button you get the result of your quiz

---
+++ script.js
@@ -0,0 +1,33 @@
+const verdictSection = document.getElementById("verdict-section");
+const getVerdictButton = document.getElementById("get-verdict-button");
+
+const question1answer = {
+  yes: document.getElementById("question1-yes"),
+  no: document.getElementById("question1-no")
+}
+
+const question2answer = {
+  yes: document.getElementById("question2-yes"),
+  no: document.getElementById("question2-no")
+}
+
+const question3answer = {
+  yes: document.getElementById("question3-yes"),
+  no: document.getElementById("question3-no")
+}
+
+getVerdictButton.addEventListener("click", function () {
+  getVerdictButton.remove();
+  const verdict = document.createElement("p");
+  verdict.style.fontWeight = 800;
+  let verdictContent;
+  if (question1answer.yes.checked && question2answer.yes.checked && (question3answer.yes.checked || question3answer.no.checked)) {
+    verdictContent = document.createTextNode("You are probably grey.");
+  } else if (question1answer.no.checked && question2answer.no.checked && question3answer.no.checked) {
+    verdictContent = document.createTextNode("You are most likely agreyosensual.");
+  } else {
+    verdictContent = document.createTextNode("You don't seem to be grey nor agroyesensual.");
+  }
+  verdict.appendChild(verdictContent);
+  verdictSection.appendChild(verdict);
+});
