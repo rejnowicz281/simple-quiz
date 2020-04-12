82aa3e4c39d84809ec4d932d406db4632e4b8c27
Alan <alanrejnowicz281@gmail.com>
2020-04-12 15:48:03 +0200

    Changed the result texts

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ getVerdictButton.addEventListener("click", function () {
   verdict.style.fontWeight = 800;
   let verdictContent;
   if (question1answer.yes.checked && question2answer.yes.checked && (question3answer.yes.checked || question3answer.no.checked)) {
-    verdictContent = document.createTextNode("You are probably grey.");
+    verdictContent = document.createTextNode("Officiis hic perspiciatis sunt similique ratione.");
   } else if (question1answer.no.checked && question2answer.no.checked && question3answer.no.checked) {
-    verdictContent = document.createTextNode("You are most likely agreyosensual.");
+    verdictContent = document.createTextNode("Placeat totam assumenda optio.");
   } else {
-    verdictContent = document.createTextNode("You don't seem to be grey nor agroyesensual.");
+    verdictContent = document.createTextNode("Deleniti ad voluptatum facilis.");
   }
   verdict.appendChild(verdictContent);
   verdictSection.appendChild(verdict);
